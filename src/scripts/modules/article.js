586d136dbdee96425324c571d5b38ab7fa0af16f
Dawid Nawrocki <dawiddeveloper@gmail.com>
2019-08-22 06:50:14 +0200

    routing basic scaffolding

--- src/scripts/modules/article.js
+++ src/scripts/modules/article.js
@@ -4,21 +4,20 @@ function mediaComponent({ author, title, article, imageUrl, id }) {
   const paragraphRegex = /<(.|\n)*?>/g;
   const lead = article.replace(paragraphRegex, '').slice(0, 300);
   const titleNoDot = title.slice(0, -1);
-  return `<article class="o-media"><div class="o-media__img"><img src="${imageUrl}"></div><div class="o-media__body"><h4>${author}</h4><h2 data-link="article/${id}">${titleNoDot}</h2><p>${lead}</p></div></article>`;
+  return `<article class="o-wrapper o-media">asfasdfasdf</article>`;
 }
 
-async function displayBlog() {
+async function displayArticle() {
   const articles = await getNewArticles();
-  let articlesList = '';
+  let article = '';
 
   for (let i = 0; i < articles.length; i++) {
     articlesList += mediaComponent(articles[i]);
   }
-
-  return articlesList;
+  return article;
 }
 
-export default displayBlog();
+export default displayArticle();
 
 // todo
 // dodac try catch / domyslne wartosci / najpierw deklaracja potem wywolanie / paginacja
--- src/scripts/modules/init.js
+++
@@ -1,7 +0,0 @@
-import displayBlog from './blog';
-
-function init() {
-  displayBlog();
-}
-
-export default init;
